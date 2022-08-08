c3e5c2beb950501d37b3d2a37bf206b3daca4d88
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-08 10:24:33 +0530

    structure created for login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,6 +12,23 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          //hello again text
+
+          //user name input text
+
+          //password input
+
+          //sign up button
+
+          //login with text
+
+          //login ways
+
+          //not a member? regiter now  text
+        ],
+      ),
     );
   }
 }
