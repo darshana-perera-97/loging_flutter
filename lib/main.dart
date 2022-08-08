a2de9385acf5aa419dd189198102d91fb2d461ad
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-08 10:23:33 +0530

    new dart file created for login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,18 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
-class LoginPage extends StatefulWidget {
-  LoginPage({Key? key}) : super(key: key);
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _LoginPageState extends State<LoginPage> {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          //hello again text
-
-          //user name input text
-
-
-          //password input
-
-
-          //sign up button
-          
-
-          //login with text
-
-
-          //login ways
-
-
-          //not a member? regiter now  text
-        ],
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
