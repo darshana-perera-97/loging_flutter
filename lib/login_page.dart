82fd608d6ad945d144fbc4f826615e8d92d522b9
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-08 10:42:10 +0530

    username, pasword text fields created

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,23 +11,79 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          //hello again text
+      backgroundColor: Colors.grey[350],
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            //hello again text
+            Center(
+                child: Text(
+              'Hello again',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            )),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+                child: Text(
+              'Welcome Back ',
+              style: TextStyle(fontSize: 20),
+            )),
+            SizedBox(
+              height: 20,
+            ),
+            //user name input text
 
-          //user name input text
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: 'User Name'),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            //password input
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: 'Password'),
+                  ),
+                ),
+              ),
+            )
 
-          //password input
+            //sign up button
 
-          //sign up button
+            //login with text
 
-          //login with text
+            //login ways
 
-          //login ways
-
-          //not a member? regiter now  text
-        ],
+            //not a member? regiter now  text
+          ],
+        ),
       ),
     );
   }
